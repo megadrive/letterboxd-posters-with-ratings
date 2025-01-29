bf98017d84fb69cdc43c32f77539413e39a35ca2
Andy <a.simeon@gmail.com>
2025-01-29 14:20:17 +1100

    feat: blitting

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,44 @@ import { fetchInfo } from "./letterboxd";
 import { template } from "./template";
 import { Jimp, loadFont } from "jimp";
 import { SANS_16_WHITE, SANS_32_WHITE } from "jimp/fonts";
+import { join } from "node:path";
+
+const MAX_WIDTH = 230;
+const MAX_HEIGHT = 345;
 
 const app = express();
 app.use(cors());
+
+const loadAssets = async () => {
+  try {
+    const banner = (
+      await Jimp.read(join(__dirname, "..", "assets", "banner.png"))
+    ).scaleToFit({ w: MAX_WIDTH, h: MAX_HEIGHT });
+    const half = (
+      await Jimp.read(join(__dirname, "..", "assets", "half.png"))
+    ).scaleToFit({
+      w: 16,
+      h: 16,
+    });
+    const star = (
+      await Jimp.read(join(__dirname, "..", "assets", "star.png"))
+    ).scaleToFit({
+      w: 16,
+      h: 16,
+    });
+
+    return { banner, half, star };
+  } catch (error) {
+    console.error("Couldn't load assets.");
+    console.error(error);
+  }
+  return undefined;
+};
+let assets: Awaited<ReturnType<typeof loadAssets>> | undefined;
+loadAssets().then((a) => {
+  assets = a;
+  console.info("Assets loaded.");
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
@@ -32,6 +67,12 @@ app.get("/:slug/info", async (req, res) => {
 });
 
 app.get("/:slug", async (req, res) => {
+  // early exit if assets are not loaded
+  if (!assets) {
+    res.status(500).send();
+    return;
+  }
+
   try {
     const { slug } = req.params;
     const info = await fetchInfo(slug);
@@ -40,9 +81,7 @@ app.get("/:slug", async (req, res) => {
       return;
     }
 
-    const poster = `${template}`
-      .replace("{{poster}}", info.poster)
-      .replace("{{rating}}", info.stars);
+    console.info({ slug, info });
 
     ///////
 
@@ -62,11 +101,48 @@ app.get("/:slug", async (req, res) => {
       rating.background = 0x000000;
 
       const compositedInitial = await Jimp.fromBuffer(posterBuffer);
-      const composited = compositedInitial.scaleToFit({ w: 230, h: 345 }).blit({
-        src: rating,
-        x: 0,
-        y: 0,
+      let composited = compositedInitial
+        .scaleToFit({ w: MAX_WIDTH, h: MAX_HEIGHT })
+        .blit({
+          src: assets.banner,
+          x: 0,
+          y: MAX_HEIGHT - assets.banner.bitmap.height,
+        });
+
+      // stars
+      const padding = 8;
+      const startX = MAX_WIDTH - assets.star.bitmap.width - padding;
+      let stars = new Jimp({
+        width: MAX_WIDTH,
+        height: assets.star.bitmap.height,
+      }).brightness(100);
+      const remainder = +info.rating % 1;
+      if (remainder > 0) {
+        stars = stars.blit({
+          src: assets.half,
+          x: MAX_WIDTH - assets.half.width - padding,
+          y: 0,
+        });
+      }
+      for (let i = 0; i < Math.floor(+info.rating); i++) {
+        const x =
+          startX -
+          i * assets.star.bitmap.width -
+          padding -
+          (remainder ? assets.half.bitmap.width : 0);
+        console.info(`Blitting star ${x}`);
+        stars = stars.blit({
+          src: assets.star,
+          x,
+          y: 0,
+        });
+      }
+      composited = composited.blit({
+        src: stars,
+        x: MAX_WIDTH - stars.bitmap.width - padding,
+        y: MAX_HEIGHT - assets.star.bitmap.height - padding,
       });
+
       return await composited.getBuffer("image/png");
     })();
     ///////
